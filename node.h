91d6866dbbab319ca94a5ff0fb13e9c8ad459403
Connor Schultz <deltaboltz@me.com>
2021-04-18 14:56:51 -0500

    Update node.h

--- node.h
+++ node.h
@@ -124,6 +124,7 @@ void node<T>::statChecker(stack<std::string, int> &stat, int scope)
     stat.push(k, scope);
 
   }
+  std::cout << "PAST <VARS> CHECK" << "\n";
   else if(key_ == "<block>")
   {
     for(int i = 0; i < children_.size(); i++)
@@ -136,6 +137,7 @@ void node<T>::statChecker(stack<std::string, int> &stat, int scope)
   }
   else if(key_ == "<R>" || key_ == "<in>" || key_ == "<assign>" || key_ == "<label>" || key_ == "<goto>")
   {
+    cout << "Inside second else if for key: " << key_ << "\n";
     for(int i = 0; i < tokens_.size(); i++)
     {
       if(tokens_[i].id == identifier)
