a03d04164cb1241482316b053e7d601d8f49c4d0
Connor Schultz <deltaboltz@me.com>
2021-04-18 17:33:35 -0500

    Update node.h

--- node.h
+++ node.h
@@ -148,7 +148,7 @@ void node<T>::statChecker(stack<std::string, int> &stat, int scope)
           std::cout << tokens_[i].instance;
           std::cout << "\" has not been defined in this scope\n";
           std::cout << "tokens that are in vector are: " << "\n";
-          for(int j = 0; j < tokens_size(); j++)
+          for(int j = 0; j < tokens_.size(); j++)
           {
             std::cout << tokens_[i].instance;
           }
